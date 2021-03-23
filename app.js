d3f3a9e39d93cb9a971ad3944d02e973af4baacc
ltebtr <univ.dog@gmail.com>
2021-03-23 22:23:46 +0900

    1022

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
 import "core-js";
+import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
-import express from "express";
-import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import { localsMiddleware } from "./middlewares";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
 const app = express();
 
-app.use(cookieParser());
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'", "http://*.fontawesome.com"],
+      scriptSrc: ["'self'", "http://*.fontawesome.com"],
+      styleSrc: ["'self'", "'unsafe-inline'"],
+      upgradeInsecureRequests: [],
+    },
+  })
+);
+app.set("view engine", "pug");
+app.use(cookieParser());
+app.use(bodyParser.json());
 app.use(morgan("dev"));
+
+app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,19 +1,14 @@
 import express from "express";
+import { join, login, logout } from "../controllers/userController";
+import { home, search } from "../controllers/videoControllers";
 import routes from "../routes";
+
 const globalRouter = express.Router();
-globalRouter.get(routes.home, (req, res) => {
-  res.send("Home");
-});
-globalRouter.get(routes.join, (req, res) => {
-  res.send("get");
-});
-globalRouter.get(routes.login, (req, res) => {
-  res.send("login");
-});
-globalRouter.get(routes.logout, (req, res) => {
-  res.send("Logout");
-});
-globalRouter.get(routes.search, (req, res) => {
-  res.send("search");
-});
+
+globalRouter.get(routes.home, home);
+globalRouter.get(routes.join, join);
+globalRouter.get(routes.login, login);
+globalRouter.get(routes.logout, logout);
+globalRouter.get(routes.search, search);
+
 export default globalRouter;
