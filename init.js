85f588f692c66868d1b8514d5c8520e740c08149
ltebtr <univ.dog@gmail.com>
2021-04-09 13:50:47 +0900

    0409

--- init.js
+++ init.js
@@ -1,8 +1,11 @@
+import dotenv from "dotenv";
 import app from "./app";
 import "./db";
-import dotenv from "dotenv";
-dotenv.config();
 import "./models/Video";
+import "./models/Comment";
+import "./models/User";
+
+dotenv.config();
 const PORT = process.env.PORT || 4000;
 const handleListening = () =>
   console.log(`✅ Listening on: http://localhost:${PORT}`);
